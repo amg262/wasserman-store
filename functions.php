8f356765151b862bed69138edd2f1181a5bf816c
Andrew Gunn <andrewmgunn26@gmail.com>
2017-12-04 01:35:49 -0600

    acf field for hide elements

--- functions.php
+++ functions.php
@@ -8,130 +8,133 @@ function wasserman_store_enqueue() {
 }
 
 function add_email_to_login() {
-    add_filter( 'gettext', 'username_change', 20, 3 );
-    function username_change( $translated_text, $text, $domain ) 
-    {
-        if ($text === 'Username') 
-        {
-            $translated_text = 'Username or Email';
-        }
-        return $translated_text;
-    }
+	add_filter( 'gettext', 'username_change', 20, 3 );
+	function username_change( $translated_text, $text, $domain ) {
+		if ( $text === 'Username' ) {
+			$translated_text = 'Username or Email';
+		}
+
+		return $translated_text;
+	}
 }
+
 //add_action( 'login_head', 'add_email_to_login' );
 
 add_action( 'after_setup_theme', 'register_user_menu' );
 //add_action( 'after_setup_theme', 'register_user_menu' );
 
 function register_user_menu() {
-  register_nav_menu( 'primary-user', __( 'Logged In Menu', 'wasserman-store' ) );
-  //add_image_size('popup_thumb', 250, 250, false);
+	register_nav_menu( 'primary-user', __( 'Logged In Menu', 'wasserman-store' ) );
+	//add_image_size('popup_thumb', 250, 250, false);
 }
 
 
-remove_action( 'woocommerce_cart_collaterals','woocommerce_cross_sell_display' );
+remove_action( 'woocommerce_cart_collaterals', 'woocommerce_cross_sell_display' );
 //add_filter('woocommerce_after_cart_totals', 'woocommerce_cross_sell_display');
 //add_action( 'widgets_init', 'wasserman_store_widgets_init' );
 
 function wasserman_store_widgets_init() {
-    register_sidebar( array(
-        'name' => __( 'Main Sidebar', 'wasserman-store' ),
-        'id' => 'sidebar-10',
-        'description' => __( 'Widgets in this area will be shown on all posts and pages.', 'theme-slug' ),
-        'before_widget' => '<li id="%1$s" class="widget %2$s">',
-	'after_widget'  => '</li>',
-	'before_title'  => '<h2 class="widgettitle">',
-	'after_title'   => '</h2>',
-    ) );
+	register_sidebar( [
+		'name'          => __( 'Main Sidebar', 'wasserman-store' ),
+		'id'            => 'sidebar-10',
+		'description'   => __( 'Widgets in this area will be shown on all posts and pages.', 'theme-slug' ),
+		'before_widget' => '<li id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</li>',
+		'before_title'  => '<h2 class="widgettitle">',
+		'after_title'   => '</h2>',
+	] );
 
-    register_sidebar( array(
-        'name' => __( 'Blog Sidebar', 'wasserman-store' ),
-        'id' => 'sidebar-20',
-        'description' => __( 'Widgets in this area will be shown on all posts and pages.', 'theme-slug' ),
-        'before_widget' => '<li id="%1$s" class="widget %2$s">',
-	'after_widget'  => '</li>',
-	'before_title'  => '<h2 class="widgettitle">',
-	'after_title'   => '</h2>',
-    ) );
+	register_sidebar( [
+		'name'          => __( 'Blog Sidebar', 'wasserman-store' ),
+		'id'            => 'sidebar-20',
+		'description'   => __( 'Widgets in this area will be shown on all posts and pages.', 'theme-slug' ),
+		'before_widget' => '<li id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</li>',
+		'before_title'  => '<h2 class="widgettitle">',
+		'after_title'   => '</h2>',
+	] );
 }
+
 /**
-* Adding ACF options page
-*/
-if( function_exists('acf_add_options_page') ) {
+ * Adding ACF options page
+ */
+if ( function_exists( 'acf_add_options_page' ) ) {
 	acf_add_options_page();
 }
 add_action( 'woocommerce_before_checkout_form', 'wsu_add_checkout_content', 12 );
 
 function wsu_add_checkout_content() {
-	
+
 	//echo 'hi';
-	if( have_rows('checkout_notices', 'options') ) {
+	if ( have_rows( 'checkout_notices', 'options' ) ) {
 
-    while ( have_rows('checkout_notices','options') ) : the_row();
+		while ( have_rows( 'checkout_notices', 'options' ) ) : the_row();
 
-	        // Your loop code
-	        $text = get_sub_field('text');
-	        $active = get_sub_field('active');
+			// Your loop code
+			$text   = get_sub_field( 'text' );
+			$active = get_sub_field( 'active' );
 
-	        
-        		echo $text;
-	        
 
-	    endwhile;
-	} 
+			echo $text;
+
+
+		endwhile;
+	}
 }
 
 /**
  * Only display minimum price for WooCommerce variable products
  **/
-add_filter('woocommerce_variable_price_html', 'display_lowest_range_prie', 10, 2);
+add_filter( 'woocommerce_variable_price_html', 'display_lowest_range_prie', 10, 2 );
 
 function display_lowest_range_prie( $price, $product ) {
 
-     $price = '';
+	$price = '';
 
 
-     $price .= woocommerce_price($product->get_price());
+	$price .= woocommerce_price( $product->get_price() );
 
-     return $price;
+	return $price;
 }
 
 // remove cross-sells from their normal place
-add_action( 'woocommerce_cart_collaterals','woocommerce_cross_sell_display',10, 2 );
+add_action( 'woocommerce_cart_collaterals', 'woocommerce_cross_sell_display', 10, 2 );
 
-/** 
+/**
  * Register new status
  * Tutorial: http://www.sellwithwp.com/woocommerce-custom-order-status-2/
-**/
+ **/
 function register_preorder_idle_order_status() {
-    register_post_status( 'wc-awaiting-shipment', array(
-        'label'                     => 'Idle Pre-Order',
-        'public'                    => true,
-        'exclude_from_search'       => false,
-        'show_in_admin_all_list'    => true,
-        'show_in_admin_status_list' => true,
-        'label_count'               => _n_noop( 'Idle Pre-Orders <span class="count">(%s)</span>', 'Idle Pre-Orders <span class="count">(%s)</span>' )
-    ) );
+	register_post_status( 'wc-awaiting-shipment', [
+		'label'                     => 'Idle Pre-Order',
+		'public'                    => true,
+		'exclude_from_search'       => false,
+		'show_in_admin_all_list'    => true,
+		'show_in_admin_status_list' => true,
+		'label_count'               => _n_noop( 'Idle Pre-Orders <span class="count">(%s)</span>', 'Idle Pre-Orders <span class="count">(%s)</span>' ),
+	] );
 }
+
 add_action( 'init', 'register_preorder_idle_order_status' );
 
 // Add to list of WC Order statuses
 function add_preorder_idle_to_order_statuses( $order_statuses ) {
 
-    $new_order_statuses = array();
+	$new_order_statuses = [];
 
-    // add new order status after processing
-    foreach ( $order_statuses as $key => $status ) {
+	// add new order status after processing
+	foreach ( $order_statuses as $key => $status ) {
 
-        $new_order_statuses[ $key ] = $status;
+		$new_order_statuses[ $key ] = $status;
 
-        if ( 'wc-processing' === $key ) {
-            $new_order_statuses['wc-idle-preorder'] = 'Idle Pre-Order';
-        }
-    }
+		if ( 'wc-processing' === $key ) {
+			$new_order_statuses['wc-idle-preorder'] = 'Idle Pre-Order';
+		}
+	}
 
-    return $new_order_statuses;
+	return $new_order_statuses;
 }
+
 add_filter( 'wc_order_statuses', 'add_preorder_idle_to_order_statuses' );
 
 add_filter( 'woocommerce_cart_no_shipping_available_html', 'wm_change_noship_message' );
@@ -139,9 +142,58 @@ add_filter( 'woocommerce_no_shipping_available_html', 'wm_change_noship_message'
 
 function wm_change_noship_message() {
 
-    if (get_field('shipping_notices', 'option')) {
-        the_field('shipping_notices', 'option');
-    } else {
-        print "Unfortunately, we are unable to calculate a shipping price for the country you selected. Please send your address, desired products and quantities to <a target='_blank' href='mailto:support@wasserman-medical.com' />support@wasserman-medical.com</a> or call <a target='_blank' href='tel:180066933337'>(800) 669-3337</a> between <strong>10:00 AM or 4:00 PM CDT.</strong>";
-    }
+	if ( get_field( 'shipping_notices', 'option' ) ) {
+		the_field( 'shipping_notices', 'option' );
+	} else {
+		print "Unfortunately, we are unable to calculate a shipping price for the country you selected. Please send your address, desired products and quantities to <a target='_blank' href='mailto:support@wasserman-medical.com' />support@wasserman-medical.com</a> or call <a target='_blank' href='tel:180066933337'>(800) 669-3337</a> between <strong>10:00 AM or 4:00 PM CDT.</strong>";
+	}
 }
+
+add_action( 'admin_footer', 'wm_hide_elements' );
+
+function wm_hide_elements() {
+	$slides = [];
+	$html   = '';
+
+	if ( have_rows( 'hide_elements', 'options' ) ) {
+
+		while ( have_rows( 'hide_elements', 'options' ) ) : the_row();
+
+			// Your loop code
+			$selector  = get_sub_field( 'selector' );
+			$property  = get_sub_field( 'property' );
+			$value     = get_sub_field( 'value' );
+			$important = get_sub_field( 'important' );
+			$active    = get_sub_field( 'active' );
+
+			if ( $active === true ) {
+
+				$slides[] = [
+					's' => $selector,
+					'p' => $property,
+					'v' => $value,
+					'a' => $active,
+					'i' => $important,
+				];
+			}
+		endwhile;
+
+		foreach ( $slides as $slide ) {
+
+			$html .= ' '. $slide['s'] . ' { ' . $slide['p'] . ' : ' . $slide['v'] . ' ; }';
+
+						/*'<style type="text/css">
+						</style>';*/
+		}
+	} ?>
+	<script>
+
+		<?php echo $html; ?>
+
+	</script>
+	<style type="text/css">
+
+		<?php echo $html; ?>
+
+	</style>
+<?php }
