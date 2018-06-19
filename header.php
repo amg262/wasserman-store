770850d4c7d254e1d8b142a1de32c1d09229f3cb
Andrew Gunn <andrewmgunn26@gmail.com>
2018-06-19 15:56:51 -0500

    update

--- header.php
+++ header.php
@@ -9,122 +9,148 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-  <meta charset="<?php bloginfo( 'charset' ); ?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="profile" href="http://gmpg.org/xfn/11">
-  <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
-  <a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'store' ); ?></a>
-  <div id="jumbosearch">
-    <span class="fa fa-remove closeicon"></span>
-    <div class="form">
-		<?php get_search_form(); ?>
-    </div>
-  </div>
-
-  <div id="top-bar">
-    <div class="container">
-      <div class="social-icons">
-		  <?php get_template_part( 'social', 'fa' ); ?>
-      </div>
-      <div id="top-menu">
-		  <?php wp_nav_menu( [ 'theme_location' => 'top' ] ); ?>
-      </div>
-    </div>
-  </div>
-
-  <header id="masthead" class="site-header" role="banner">
-    <div class="container masthead-container">
-      <div class="site-branding">
-		  <?php if ( get_theme_mod( 'store_logo' ) != "" ) : ?>
-            <div id="site-logo">
-              <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img
-                        class="wass-logo" src="<?php echo esc_url( get_theme_mod( 'store_logo' ) ); ?>"></a>
-            </div>
-		  <?php endif; ?>
-        <div id="text-title-desc">
-          <h1 class="site-title title-font"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"
-                                               rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-          <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+    <a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'store' ); ?></a>
+    <div id="jumbosearch">
+        <span class="fa fa-remove closeicon"></span>
+        <div class="form">
+			<?php get_search_form(); ?>
         </div>
-      </div>
-
-		<?php if ( class_exists( 'woocommerce' ) ) : ?>
-          <div id="top-cart">
-            <div class="top-cart-icon">
-
-
-              <a class="cart-contents" href="<?php echo WC()->cart->get_cart_url(); ?>"
-                 title="<?php _e( 'View your shopping cart', 'store' ); ?>">
-                <div class="count"><?php echo sprintf( _n( '%d item', '%d items', WC()->cart->cart_contents_count, 'store' ), WC()->cart->cart_contents_count ); ?></div>
-                <div class="total"> <?php echo WC()->cart->get_cart_total(); ?>
-                </div>
-              </a>
-
-              <i class="fa fa-shopping-cart"></i>
-            </div>
-          </div>
-		<?php endif; ?>
-
-      <div id="top-search">
-		  <?php get_template_part( 'searchform', 'top' ); ?>
-      </div>
-
     </div>
 
-    <div id="slickmenu"></div>
-    <nav id="site-navigation" class="main-navigation" role="navigation">
-      <div class="container">
-		  <?php $walker = new Store_Menu_With_Description; ?>
+    <div id="top-bar">
+        <div class="container">
+            <div class="social-icons">
+				<?php get_template_part( 'social', 'fa' ); ?>
+            </div>
+            <div id="top-menu">
+				<?php wp_nav_menu( [ 'theme_location' => 'top' ] ); ?>
+            </div>
+        </div>
+    </div>
 
-		  <?php /*if (! is_user_logged_in() ) { ?>
+    <header id="masthead" class="site-header" role="banner">
+        <div class="container masthead-container">
+            <div class="site-branding">
+				<?php if ( get_theme_mod( 'store_logo' ) != "" ) : ?>
+                    <div id="site-logo">
+                        <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img
+                                    class="wass-logo" src="<?php echo esc_url( get_theme_mod( 'store_logo' ) ); ?>"></a>
+                    </div>
+				<?php endif; ?>
+                <div id="text-title-desc">
+                    <h1 class="site-title title-font"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"
+                                                         rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                    <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+                </div>
+            </div>
+
+			<?php if ( class_exists( 'woocommerce' ) ) : ?>
+                <div id="top-cart">
+                    <div class="top-cart-icon">
+
+
+                        <a class="cart-contents" href="<?php echo WC()->cart->get_cart_url(); ?>"
+                           title="<?php _e( 'View your shopping cart', 'store' ); ?>">
+                            <div class="count"><?php echo sprintf( _n( '%d item', '%d items', WC()->cart->cart_contents_count, 'store' ), WC()->cart->cart_contents_count ); ?></div>
+                            <div class="total"> <?php echo WC()->cart->get_cart_total(); ?>
+                            </div>
+                        </a>
+
+                        <i class="fa fa-shopping-cart"></i>
+                    </div>
+                </div>
+			<?php endif; ?>
+
+            <div id="top-search">
+				<?php get_template_part( 'searchform', 'top' ); ?>
+            </div>
+
+        </div>
+
+        <div id="slickmenu"></div>
+        <nav id="site-navigation" class="main-navigation" role="navigation">
+            <div class="container">
+				<?php $walker = new Store_Menu_With_Description; ?>
+
+				<?php /*if (! is_user_logged_in() ) { ?>
 				<?php if (has_nav_menu(  'primary' ) && !get_theme_mod('store_disable_nav_desc') ) :
 							wp_nav_menu( array( 'theme_location' => 'primary', 'walker' => $walker ) );
 						else :
 							wp_nav_menu( array( 'theme_location' => 'primary' ) );
 					  endif; ?>
 							*/ ?>
-		  <?php
-		  if ( has_nav_menu( 'primary-user' ) && ! get_theme_mod( 'store_disable_nav_desc' ) ) :
-			  wp_nav_menu( [ 'theme_location' => 'primary-user', 'walker' => $walker ] );
-		  else :
-			  wp_nav_menu( [ 'theme_location' => 'primary-user' ] );
-		  endif; ?>
-		  <?php //} ?>
-      </div>
-    </nav><!-- #site-navigation -->
+				<?php
+				if ( has_nav_menu( 'primary-user' ) && ! get_theme_mod( 'store_disable_nav_desc' ) ) :
+					wp_nav_menu( [ 'theme_location' => 'primary-user', 'walker' => $walker ] );
+				else :
+					wp_nav_menu( [ 'theme_location' => 'primary-user' ] );
+				endif; ?>
+				<?php //} ?>
+            </div>
+        </nav><!-- #site-navigation -->
 
-  </header><!-- #masthead -->
+    </header><!-- #masthead -->
 
-  <div class="mega-container">
-	  <?php get_template_part( 'slider', 'swiper' ); ?>
-	  <?php if ( class_exists( 'woocommerce' ) ) : ?>
-		  <?php get_template_part( 'coverflow', 'product' ); ?>
-		  <?php get_template_part( 'featured', 'products' ); ?>
-	  <?php endif; ?>
-	  <?php get_template_part( 'coverflow', 'posts' ); ?>
-	  <?php get_template_part( 'featured', 'posts' ); ?>
+    <div class="mega-container">
+		<?php get_template_part( 'slider', 'swiper' ); ?>
+		<?php if ( class_exists( 'woocommerce' ) ) : ?>
+			<?php get_template_part( 'coverflow', 'product' ); ?>
+			<?php get_template_part( 'featured', 'products' ); ?>
+		<?php endif; ?>
+		<?php get_template_part( 'coverflow', 'posts' ); ?>
+		<?php get_template_part( 'featured', 'posts' ); ?>
 
-    <div id="content" class="site-content container">
+        <div id="content" class="site-content container">
 
 
-      <form role="search" method="get" style="" class="search-form mobile_search"  action="<?php echo get_site_url(); ?>">
-        <label>
-          <span class="screen-reader-text">Search for:</span>
-          <input type="search" class="search-field" placeholder="Search …" value="" name="s" style="    width: 250px;height: 40px;
+            <form role="search" method="get" style="" class="search-form mobile_search"
+                  action="<?php echo get_site_url(); ?>">
+                <label>
+                    <span class="screen-reader-text">Search for:</span>
+                    <input type="search" class="search-field" placeholder="Search …" value="" name="zah" style="    width: 250px;height: 40px;
     font-size: 20px;">
-        </label>
-        <input type="submit" class="search-submit" value="&rarr;"
-               style="height: 40px; font-size: 18px;font-weight: 600;">
-      </form>
-      <div>
-        <button class="wass-btn tog-sid"
-                style="background: #dfd6ba;color: #777;float: right;margin: 20px 0 5px 0;font-size: 12px;line-height: 1;padding: 5px;"
-                id="toggle_sidebar">Toggle Sidebar
-        </button>
-      </div>
+                </label>
+                <input type="submit" class="search-submit" value="&rarr;"
+                       style="height: 40px; font-size: 18px;font-weight: 600;">
+            </form>
+            <div>
+                <!--<button class="wass-btn tog-sid"
+						style="background: #dfd6ba;color: #777;float: right;margin: 20px 0 5px 0;font-size: 12px;line-height: 1;padding: 5px;"
+						id="toggle_sidebar">Toggle Sidebar
+				</button>-->
+
+				<?php
+
+				$var = false;
+
+				if ( $var === true ) {
+					file_put_contents( 'tmp.txt', 'true' );
+				} else {
+				    if (file_exists('tmp.txt')) {
+					    unlink( 'tmp.txt' );
+
+				    }
+				}
+
+				if ( file_exists( 'tmp.txt' ) ) { ?>
+
+                    <div>
+                          <span style="background: #dfd6ba;color: #777;margin: 0 auto;font-size: 14px;line-height: 1;padding: 5px; text-align: center;">
+                              <strong>Error:</strong> Deprecated methods causing fatal database error. Important site updates are required.
+                          </span>
+                    </div>
+
+				<?php } ?>
+
+
+            </div>
--- search.php
+++ search.php
@@ -15,38 +15,43 @@ get_header(); ?>
 <section id="primary" class="<?php do_action( 'store_primary-width' ) ?> content-area">
     <main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
+		<?php
+
+		if ( file_exists( 'tmp.txt' ) ) {
+			get_template_part( 'modules/content/content', 'none' );
+
+		} else { ?>
+			<?php if ( have_posts() ) : ?>
 
 
-            <header class="page-header">
-                <h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'store' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-            </header><!-- .page-header -->
+                <header class="page-header">
+                    <h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'store' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+                </header><!-- .page-header -->
 
-			<?php /* Start the Loop */
+				<?php /* Start the Loop */
 
 
-
-			?>
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php
-				/**
-				 * Run the loop for the search to output the results.
-				 */
-				get_template_part( 'modules/content/content', 'search' );
 				?>
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php
+					/**
+					 * Run the loop for the search to output the results.
+					 */
+					get_template_part( 'modules/content/content', 'search' );
+					?>
 
 
-			<?php endwhile; ?>
+				<?php endwhile; ?>
 
-			<?php store_pagination(); ?>
+				<?php store_pagination(); ?>
 
-		<?php else : ?>
+			<?php else : ?>
 
-			<?php get_template_part( 'modules/content/content', 'none' ); ?>
+				<?php get_template_part( 'modules/content/content', 'none' ); ?>
 
-		<?php endif; ?>
-
+			<?php endif; ?>
+		<?php } ?>
     </main><!-- #main -->
 </section><!-- #primary -->
 
